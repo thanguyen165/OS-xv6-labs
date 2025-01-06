dd3f9c7c6e9d69cb28ab0f3f629453cb63e2258f
thanguyen165 <thanguyen165@gmail.com>
2025-01-06 21:07:56 +0700

    feat: add Lab02 files - lab syscall - tracing
    modify kernel and user
    
    diff file in docs/release/Lab02_trace.patch

--- kernel/proc.c
+++ kernel/proc.c
@@ -169,6 +169,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->tracemask = 0;
 }
 
 // Create a user page table for a given process, with no user memory,
@@ -311,6 +312,9 @@ fork(void)
   safestrcpy(np->name, p->name, sizeof(p->name));
 
   pid = np->pid;
+
+  // copy trace mask from parent process to child process
+  np->tracemask = p->tracemask;
 
   release(&np->lock);
 
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,15 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_trace(void)
+{
+  int mask;
+  argint(0, &mask);
+  if(mask < 0){
+    return -1;
+  }
+  myproc()->tracemask = mask;
+  return 0;
+}
